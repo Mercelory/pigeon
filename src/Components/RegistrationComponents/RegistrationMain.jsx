8d23ec703112ef4a2b9656eeadca0a3f924a927a
mercelory <mercelory@gmail.com>
2025-03-03 16:33:12 +0200

    google auth

--- src/Components/RegistrationComponents/RegistrationMain.jsx
+++ src/Components/RegistrationComponents/RegistrationMain.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuth } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
 import PigeonIco from "../../img/Subtract.svg"
+import { TbBrandGoogle} from "react-icons/tb";
 
 const Register = () => {
   const [Name, setName] = useState("");
@@ -18,27 +19,33 @@ const Register = () => {
   return (
     <section className="flex bg-linear-to-r from-slate-200 to-white">
       <div className="flex justify-center items-center flex-col w-screen h-screen">
-      <img src ={PigeonIco} className="w-15 mb-10 "></img>
-      <h1 className="font-bold text-3xl mb-3 text-slate-500">Registration</h1>
-      <form onSubmit={handleRegister} className="flex flex-col">
+        <div className=" rounded-xl px-10 ">
+      <img src ={PigeonIco} className="w-15"></img>
+      </div>
+      <h1 className="font-bold text-3xl mb-3 text-slate-500 mt-5">Create account</h1>
+      <form onSubmit={handleRegister} className="flex flex-col w-1/5">
         <input
           type="text"
-          placeholder="Name"
+          placeholder="Enter your name"
           value={Name}
           onChange={(e) => setName(e.target.value)}
           required
-           className="mb-3 placeholder:text-neutral-400 rounded-md border-1 p-2 border-neutral-400 hover:border-neutral-900 transition-color duration-300"/>
+           className="z-1 mb-3 placeholder:text-neutral-400 rounded-md p-2 bg-slate-200 transition-color duration-300"/>
+
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="mb-3 rounded-md placeholder:text-neutral-400 border-1 p-2 border-neutral-400 hover:border-neutral-900 transition-color duration-300"/>
+          className="mb-3 rounded-md placeholder:text-neutral-400 bg-slate-200 p-2 transition-color duration-300"/>
         <button type="submit" className="rounded-md border-1 border-neutral-400 p-1 w-full bg-slate-500 text-white transition-color duration-300">Confirm</button>
       </form>
       <h2 className="text-neutral-400 mt-3 font-sm">Already registered? <a href = '#' className="font-bold text-slate-500">Sign In</a></h2>
+      <div className="border-1 border-slate-300 w-1/5 mt-5"></div>
+      <a href='#' className="bg-slate-200 w-1/5 flex justify-start items-center mt-5 h-12 rounded-md p-2 text-slate-500"><TbBrandGoogle className="text-3xl font-bold mr-3 text-slate-400"/>Sign in via Google</a>
     </div>
+
     </section>
   );
 };
