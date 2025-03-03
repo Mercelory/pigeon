7bedce3b7c6e6c9fd7f0182f7c925001cb3dd507
mercelory <mercelory@gmail.com>
2025-03-03 16:46:40 +0200

    responsive registration menu fix

--- src/Components/RegistrationComponents/RegistrationMain.jsx
+++ src/Components/RegistrationComponents/RegistrationMain.jsx
@@ -19,11 +19,11 @@ const Register = () => {
   return (
     <section className="flex bg-linear-to-r from-slate-200 to-white">
       <div className="flex justify-center items-center flex-col w-screen h-screen">
-        <div className=" rounded-xl px-10 ">
+        <div className=" rounded-xl px-10">
       <img src ={PigeonIco} className="w-15"></img>
       </div>
       <h1 className="font-bold text-3xl mb-3 text-slate-500 mt-5">Create account</h1>
-      <form onSubmit={handleRegister} className="flex flex-col w-1/5">
+      <form onSubmit={handleRegister} className="flex flex-col">
         <input
           type="text"
           placeholder="Enter your name"
@@ -42,8 +42,8 @@ const Register = () => {
         <button type="submit" className="rounded-md border-1 border-neutral-400 p-1 w-full bg-slate-500 text-white transition-color duration-300">Confirm</button>
       </form>
       <h2 className="text-neutral-400 mt-3 font-sm">Already registered? <a href = '#' className="font-bold text-slate-500">Sign In</a></h2>
-      <div className="border-1 border-slate-300 w-1/5 mt-5"></div>
-      <a href='#' className="bg-slate-200 w-1/5 flex justify-start items-center mt-5 h-12 rounded-md p-2 text-slate-500"><TbBrandGoogle className="text-3xl font-bold mr-3 text-slate-400"/>Sign in via Google</a>
+      <div className="border-1 border-slate-300 w-15 mt-5"></div>
+      <a href='#' className="bg-slate-200 flex justify-start items-center mt-5 h-12 rounded-md p-2 text-slate-500"><TbBrandGoogle className="text-3xl font-bold mr-3 text-slate-400"/>Sign in via Google</a>
     </div>
 
     </section>
